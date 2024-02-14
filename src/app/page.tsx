f48436070fb782590d92a2ad35c7dadafa39094d
Piripe <jeuxdepierre@laposte.net>
2024-02-14 21:33:43 +0100

    Removed timer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,10 @@ import RizzDisplay from "@/components/RizzDisplay";
 import styles from "./page.module.scss";
 import CalendarOverlay from "@/components/overlay/CalendarOverlay";
 import { getLayoutAtDate } from "@/db";
-import Timer from "./Timer";
 
 //export const revalidate = 30
 
 export default async function Page() {
-    if (Date.now() - new Date(2024, 1, 14, 20, 0).valueOf() < 0) return <Timer/>; //new Date(2024,1,14,20,0).valueOf()
-
     return (
         <>
             <div className={styles.display}>
