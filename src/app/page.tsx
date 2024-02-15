9b9f98f2a41bae03b1c7d157e68bc11d78eff461
Piripe <jeuxdepierre@laposte.net>
2024-02-15 17:12:06 +0100

    Fixed Layout caching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,16 @@
-"use server";
-
 import RizzDisplay from "@/components/RizzDisplay";
 import styles from "./page.module.scss";
 import CalendarOverlay from "@/components/overlay/CalendarOverlay";
 import { getLayoutAtDate } from "@/db";
+import { cache } from "react";
 
-//export const revalidate = 30
+export const revalidate = 60
 
 export default async function Page() {
     return (
         <>
             <div className={styles.display}>
-                {RizzDisplay(await getLayoutAtDate(Math.floor((Date.now()+1000*60*60*1) / (1000 * 60 * 60 * 24))))}
+                {RizzDisplay(await getRizz())}
             </div>
             <div className={styles.overlay}>
                 <CalendarOverlay />
@@ -19,3 +18,8 @@ export default async function Page() {
         </>
     );
 }
+
+const getRizz = cache(async () => {
+    const item = await getLayoutAtDate(Math.floor((Date.now()+1000*60*60*1) / (1000 * 60 * 60 * 24)))
+    return item
+  })
